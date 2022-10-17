30f5ba12fa242ea3d997cf23b4b569aaf63e6fc2
Vrubcova3 <114923410+VikaRubcova@users.noreply.github.com>
2022-10-17 11:59:22 +0000

    3uzd

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-sk1=int(input("Ievadiet skaitli: "))
-if sk1>10:
-  print("Skaitlis ir liels")
-else:
-  print("Skailis ir mazs")
+x=int(input("Введи x: "))
+y=int(input("Введи y: "))
+if x==0 and y==0:
+ print("точка находится в начале оси координат")
+if x==0:
+ print("точка находится на оси y")
+if y==0:
+ print("точка находится на оси x")
+if x>0 and y>0: print("1 квадрант")
+elif x<0 and y>0: print("2 квадрант")
+elif x<0 and y<0: print("3 квадрант")
+elif x>0 and y<0: print("4 квадрант")
